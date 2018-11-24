009b90a978ace6e183f116272ecd62cfd4355132
fwang1703 <40136329+fwang1703@users.noreply.github.com>
2018-11-24 22:30:07 +1100

    Add files via upload

---
+++ multiples35js.js
@@ -0,0 +1,21 @@
+// Multiples of 3 and 5
+
+// Declaring variables
+let sum = 0;
+let  i = 1;
+
+// While loop sorting through values from 1-1000
+while (i < 1000)
+{
+// Checking if number is multiple of 3 or 5 and summing multiples
+  if (i % 3 == 0 || i % 5 == 0)
+  {
+	  sum+=i;
+    i++;
+  }
+  else
+  {
+    i++;
+  }
+}
+console.log(sum);
